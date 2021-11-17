2d35a5bec854c7d7a121d2c26e3e7220bb69cdf1
KMordovets <88400354+KMordovets@users.noreply.github.com>
2021-11-17 11:24:33 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Web;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace project_1
+{
+
+    public class Data {
+        public String id { get; set; }
+        public bool display { get; set; }
+        public bool entities { get; set; }
+        public String fields { get; set; }
+        public bool member { get; set; }
+    }
+
+    class Program
+    {
+        private static object args;
+
+        static void Main(string[] args)
+        {
+            TrelloRequestAsync("https://trello.com/b/I5Yr4tAu/software-management-automatization");
+        }
+
+        static async Task<string> TrelloRequestAsync(string url)
+        {
+            var tcs = new TaskCompletionSource<string>();
+            try
+            {
+                var client = new RestClient(url);
+                client.GetAsync(new RestRequest(), (response, handle) =>
+                {
+                    if ((int)response.StatusCode >= 400)
+                    {
+                        tcs.SetException(new Exception(response.StatusDescription));
+                    }
+                    else
+                    {
+                        tcs.SetResult(response.Content);
+                    }
+                });
+            }
+            catch (Exception ex)
+            {
+                tcs.SetException(ex);
+            }
+
+            //catch (InvalidOperationException e)
+            //{
+
+            //}
+            //catch (HttpRequestException e)
+            //{
+
+            //}
+            //catch (TaskCanceledException e)
+            //{
+
+            //}
+
+            return await tcs.Task;
+        }
+
+        private static Task PostAsJsonAsync(string url, string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

e0f14a39f7d4cbd4c89452efad9cd7cb35c64fd6
KMordovets <88400354+KMordovets@users.noreply.github.com>
2024-10-16 18:26:20 +0300

    Убран избыточный код

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
+using System.Net.Http;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,36 +10,94 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Markup;
 
-namespace TestMVVM
+namespace ProjectClosureToolMVVM
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        private int i = 1;
-        //private string textValue = "";
-        //private int randomValue1;
-        //private int randomValue2;
-        //private int sum;
-
-
         public MainWindow()
         {
             InitializeComponent();
             DataContext = new WindowBind();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        // Board code
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
         {
-            ((WindowBind)DataContext).WindowBindButton(i);
-            i++;
+
         }
 
-        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        // Key
+        private void TextBox_TextChanged_2(object sender, TextChangedEventArgs e)
         {
-            
+
+        }
+
+        // Token
+        private void TextBox_TextChanged_3(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        // Download
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ((WindowBind)DataContext).WindowBindDownload();
+        }
+
+        private void DataGridLabels_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int iInputIgnore = DataGridLabels.Items.IndexOf(DataGridLabels.CurrentItem);
+            int iColumnIndex = DataGridLabels.Items.IndexOf(DataGridLabels.CurrentItem);
+            //((WindowBind)DataContext).CombinationsDownloadCheckbox(iInputIgnore, iColumnIndex);
+            //((WindowBind)DataContext).CombinationsDownload();
+            if ((iInputIgnore <= Download.labelsList.Count) && (iColumnIndex == 1) && iInputIgnore >= 0)
+            {
+                foreach (TrelloObjectLabels aLabel in Download.labelsList)
+                    if (aLabel.CardID.Equals(iInputIgnore))
+                    {
+                        if (Download.CheckIgnored(aLabel.CardLabel))
+                        {
+                            Download.ignoredLabelsList.Remove(aLabel);
+                            DataGridLabels.CurrentItem = false;
+                            //DataGridLabels.CurrentCell.Value = false;
+                            if (Download.ignoredLabelsList.Count == 0)
+                                Download.ignoredLabelsListFilled = false;
+                        }
+                        else
+                        {
+                            Download.ignoredLabelsList.Add(aLabel);
+                            DataGridLabels.CurrentItem = true;
+                            //DataGridLabels.CurrentCell.Value = true;
+                            Download.ignoredLabelsListFilled = true;
+                        }
+                    }
+                ((WindowBind)DataContext).CombinationsDownloadCheckbox(iInputIgnore, iColumnIndex);
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            ((WindowBind)DataContext).CombinationsDownload();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            ((WindowBind)DataContext).Export();
+        }
+
+        private void DataGridResults_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            ((WindowBind)DataContext).Results();
         }
 
         private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -49,22 +105,9 @@ namespace TestMVVM
 
         }
 
-        //public class Table
-        //{
-        //    public string Text { get; set; }
-        //    //public int Random1 { get; set; }
-        //    //public int Random2 { get; set; }
-        //    //public int Sum { get; set; }
-        //}
+        private void DataGridCombinations_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
 
-        //public void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        //{
-        //    textValue = $"{i}. {e}";
-        //    i++;
-        //    var rand = new Random();
-        //    randomValue1 = rand.Next();
-        //    randomValue2 = rand.Next();
-        //    sum = randomValue1 + randomValue2;
-        //}
+        }
     }
 }
--- WindowBind.cs
+++ WindowBind.cs
@@ -1,39 +1,236 @@
-﻿using System;
+﻿using DocumentFormat.OpenXml.Office2010.Word;
+using DocumentFormat.OpenXml.Presentation;
+using OfficeOpenXml.FormulaParsing.Excel.Functions.Database;
+using OfficeOpenXml.FormulaParsing.Excel.Functions.Math;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Configuration;
 using System.Linq;
+using System.Net.Http;
+using System.Reflection.Emit;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+using static System.Net.Mime.MediaTypeNames;
 
-namespace TestMVVM
+namespace ProjectClosureToolMVVM
 {
-    internal class WindowBind : INotifyPropertyChanged
+    internal class WindowBind: INotifyPropertyChanged
     {
+        public ICommand MyCommand { get; set; }
+
         public event PropertyChangedEventHandler? PropertyChanged;
-        ObservableCollection<Model> models = new ObservableCollection<Model>();
+        public ObservableCollection<Model> labelModels = new ObservableCollection<Model>();
+        public ObservableCollection<Model> unitModels = new ObservableCollection<Model>();
+        public ObservableCollection<Model> combinationModels = new ObservableCollection<Model>();
+        public ObservableCollection<ResultsModel> resultsModels = new ObservableCollection<ResultsModel>();
 
         public void OnPropertyChanged(string propertyName)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
-        public ObservableCollection<Model> Models { get { return models; } set { models = value; OnPropertyChanged("Models"); } }
-        
+        public ObservableCollection<Model> LabelModels { get { return labelModels; } set { labelModels = value; OnPropertyChanged("LabelModels"); } }
+        public ObservableCollection<Model> UnitModels { get { return unitModels; } set { unitModels = value; OnPropertyChanged("UnitModels"); } }
+        public ObservableCollection<Model> CombinationModels { get { return combinationModels; } set { combinationModels = value; OnPropertyChanged("UnitModels"); } }
+        public ObservableCollection<ResultsModel> ResultsModels { get { return resultsModels; } set { resultsModels = value; OnPropertyChanged("ResultModels"); } }
+
         public WindowBind()
         {
-            Models = new ObservableCollection<Model>();
+            LabelModels = new ObservableCollection<Model>();
+            UnitModels = new ObservableCollection<Model>();
+            CombinationModels = new ObservableCollection<Model>();
         }
 
-        public void WindowBindButton(int i)
+        private string? boardCode;
+        public string BoardCode { get => boardCode; set { boardCode = value; OnPropertyChanged("BoardCode"); } }
+        private string? apiKey;
+        public string APIKey { get => apiKey; set { apiKey = value; OnPropertyChanged("APIKey"); } }
+        private string? myTrelloToken;
+        public string MyTrelloToken { get => myTrelloToken; set { myTrelloToken = value; OnPropertyChanged("MyTrelloToken"); } }
+
+        public void WindowBindDownload()
         {
-            Models.Clear();
-            for (int j = 0; j<100; j++) 
+            Download.ClearLabels();
+            Download.labelsListFilled = false;
+            Download.unitsListFilled = false;
+            Download.ignoredLabelsList.Clear();
+            Download.ignoredLabelsListFilled = false;
+            Download.selectedUnits.Clear();
+            Download.selectedCombinations.Clear();
+            LabelModels.Clear();
+            UnitModels.Clear();
+            CombinationModels.Clear();
+            ResultsModels.Clear();
+            if (BoardCode != API_Req.boardCode && BoardCode != "" && BoardCode != null)
+                { API_Req.boardCode = BoardCode; }
+            if (APIKey != "Default" && APIKey != "" && APIKey != null)
+                { API_Req.APIKey = APIKey; }
+            if (myTrelloToken != "Default" && myTrelloToken != "" && myTrelloToken != null)
+                { API_Req.myTrelloToken = myTrelloToken; }
+            API_Req.RequestAsync(API_Req.APIKey, API_Req.myTrelloToken, API_Req.CardFilter, API_Req.CardFields, API_Req.boardCode);
+            Download.BoardM();
+            foreach (TrelloObjectLabels aLabel in Download.labelsList)
+                LabelModels.Add(new Model(aLabel.CardLabel, Download.CheckIgnored(aLabel.CardLabel)));
+            if (Download.unitsListFilled)
             {
-                Models.Add(new Model(Text, j));
+                Download.distinctUnits = Download.units.Distinct();
+                Download.distinctUnitsList = Download.distinctUnits.ToList();
+                Download.distinctUnitsList.Sort();
+                foreach (string aUnit in Download.distinctUnitsList)
+                    UnitModels.Add(new Model(aUnit, Download.selectedUnits.Contains(aUnit)));
             }
-            //Models.Add(new Model { Text = $"{i}. {text}" });
+            CombinationsDownload();
+        }
+
+        public void CombinationsDownload()
+        {
+            Download.ignoredLabelsList.Clear();
+            Download.ignoredLabelsListFilled = false;
+            Download.selectedCombinations.Clear();
+            CombinationModels.Clear();
+            ResultsModels.Clear();
+            foreach (Model aLabel in LabelModels)
+                if (aLabel.IsChecked)
+                {
+                    Download.ignoredLabelsList.Add(new TrelloObjectLabels()
+                    {
+                        CardID = Download.ignoredLabelsList.Count,
+                        CardLabel = aLabel.Label
+                    });
+                    Download.ignoredLabelsListFilled = true;
+                }
+            Download.LabelCombinationsI();
+            foreach (string aCombination in Download.distinctCombinationsListI)
+                CombinationModels.Add(new Model(aCombination, Download.selectedCombinations.Contains(aCombination)));
+            Trl.sums.Clear();
+            for (int i = 0; i < Download.distinctUnitsList.Count(); i++)
+                for (int j = 0; j < Download.distinctCombinationsListI.Count(); j++)
+                    Trl.Sum(i, j + 1);
+
+            //____________________________________________________________________
+            //Download.ignoredLabelsList.Clear();
+            //Download.ignoredLabelsListFilled = false;
+            //Download.selectedCombinations.Clear();  // 01 блужд Sel Comb
+            //CombinationModels.Clear();
+            //ResultsModels.Clear();
+            //foreach (Model aLabel in LabelModels)
+            //    if (aLabel.IsChecked)
+            //    {
+            //        Download.ignoredLabelsList.Add(new TrelloObjectLabels() 
+            //        {
+            //            CardID=Download.ignoredLabelsList.Count,
+            //            CardLabel= aLabel.Label
+            //        });
+            //        Download.ignoredLabelsListFilled = true;
+            //    }
+            //Download.LabelCombinationsI();
+            //foreach (string aCombination in Download.distinctCombinationsListI)
+            //    CombinationModels.Add(new Model(aCombination, Download.selectedCombinations.Contains(aCombination)));
+            //Trl.sums.Clear();
+            //for (int i = 0; i < Download.distinctUnitsList.Count(); i++)
+            //    for (int j = 0; j < Download.distinctCombinationsListI.Count(); j++)
+            //        Trl.Sum(i, j + 1);
+
+            //foreach (TrelloObjectSums aSum in Trl.sums)
+            //    if (Download.selectedUnits.Contains(aSum.CardUnit) && Download.selectedCombinations.Contains(aSum.LabelCombinationI))
+
+            //        ResultsModels.Add(new ResultsModel(aSum.CardUnit, aSum.LabelCombinationI, aSum.SumEstimate, aSum.SumPoint));
+        }
+
+        public void CombinationsDownloadCheckbox(int rowI, int columnI)
+        {
+            Download.ignoredLabelsList.Clear();
+            Download.ignoredLabelsListFilled = false;
+            Download.selectedCombinations.Clear();
+            CombinationModels.Clear();
+            ResultsModels.Clear();
+            //if ((rowI <= Download.labelsList.Count) && (columnI == 1) && rowI >= 0)
+            //{
+            //    foreach (TrelloObjectLabels aLabel in Download.labelsList)
+            //        if (aLabel.CardID.Equals(rowI))
+            //        {
+            //            if (Download.CheckIgnored(aLabel.CardLabel))
+            //            {
+            //                Download.ignoredLabelsList.Remove(aLabel);
+            //                MainWindow.DataGridLabels.CurrentCell.Value = false;
+            //                if (Download.ignoredLabelsList.Count == 0)
+            //                    Download.ignoredLabelsListFilled = false;
+            //            }
+            //            else
+            //            {
+            //                Download.ignoredLabelsList.Add(aLabel);
+            //                MainWindow.DataGridLabels.CurrentCell.Value = true;
+            //                Download.ignoredLabelsListFilled = true;
+            //            }
+            //        }
+            //}
+
+            //foreach (Model aLabel in LabelModels)
+            //    if (aLabel.IsChecked)
+            //    {
+            //        Download.ignoredLabelsList.Add(new TrelloObjectLabels()
+            //        {
+            //            CardID = Download.ignoredLabelsList.Count,
+            //            CardLabel = aLabel.Label
+            //        });
+            //        Download.ignoredLabelsListFilled = true;
+            //    }
+            //Download.LabelCombinationsI();
+            //foreach (string aCombination in Download.distinctCombinationsListI)
+            //    CombinationModels.Add(new Model(aCombination, Download.selectedCombinations.Contains(aCombination)));
+            //Trl.sums.Clear();
+            //for (int i = 0; i < Download.distinctUnitsList.Count(); i++)
+            //    for (int j = 0; j < Download.distinctCombinationsListI.Count(); j++)
+            //        Trl.Sum(i, j + 1);
+        }
+
+        public void Results()
+        {
+            ResultsModels.Clear();
+            Download.selectedUnits.Clear();
+            foreach (Model aUnit in UnitModels)
+                if (aUnit.IsChecked && !Download.selectedUnits.Contains(aUnit.Label))
+                    Download.selectedUnits.Add(aUnit.Label);
+            Download.selectedCombinations.Clear();
+            foreach (Model aCombination in CombinationModels)
+                if (aCombination.IsChecked && !Download.selectedCombinations.Contains(aCombination.Label))
+                    Download.selectedCombinations.Add(aCombination.Label);
+            Trl.sums.Clear();
+            for (int i = 0; i < Download.distinctUnitsList.Count(); i++)
+                for (int j = 0; j < Download.distinctCombinationsListI.Count(); j++)
+                    Trl.Sum(i, j + 1);
+            foreach (TrelloObjectSums aSum in Trl.sums)
+                if (Download.selectedUnits.Contains(aSum.CardUnit) && Download.selectedCombinations.Contains(aSum.LabelCombinationI))
+                    ResultsModels.Add(new ResultsModel(aSum.CardUnit, aSum.LabelCombinationI, aSum.SumEstimate, aSum.SumPoint));
+        }
+
+        public void Export()
+        {
+            if (Download.labelsListFilled && Download.unitsListFilled && Download.distinctCombinationsListFilled)
+            {
+                ResultsModels.Clear();
+                Download.selectedUnits.Clear();
+                foreach (Model aUnit in UnitModels)
+                    if (aUnit.IsChecked && !Download.selectedUnits.Contains(aUnit.Label))
+                        Download.selectedUnits.Add(aUnit.Label);
+                Download.selectedCombinations.Clear();
+                foreach (Model aCombination in CombinationModels)
+                    if (aCombination.IsChecked && !Download.selectedCombinations.Contains(aCombination.Label))
+                        Download.selectedCombinations.Add(aCombination.Label);
+                Trl.sums.Clear();
+                foreach (string aUnit in Download.distinctUnitsList)
+                    foreach (string aCombination in Download.distinctCombinationsListI)
+                //System.InvalidOperationException:
+                //"Collection was modified; enumeration operation may not execute."
+                        Trl.Sum(Download.distinctUnitsList.IndexOf(aUnit), Download.distinctCombinationsListI.IndexOf(aCombination) + 1);
+                TableResp.FillExcel();
+            }
         }
 
         protected bool SetProperty<T>(ref T field, T newValue, [CallerMemberName] string? propertyName = null)
@@ -46,9 +243,5 @@ namespace TestMVVM
             }
             return false;
         }
-
-        private string? text;
-
-        public string Text { get => text; set => SetProperty(ref text, value); }
     }
 }

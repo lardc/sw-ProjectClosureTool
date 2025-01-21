752c12f710c03c0f2aace19d20f82ccea7efeebf
KMordovets <kmordovets@yandex.ru>
2025-01-21 21:04:47 +0300

    Фильтрация коллекции
    Некорректная версия

--- Model.cs
+++ Model.cs
@@ -61,15 +61,28 @@ namespace ProjectClosureToolMVVM
 
         public bool IsChecked
         {
+            //get { return isChecked; }
+            //set
+            //{
+            //    if (isChecked == value) return;
+            //    isChecked = value;
+            //    OnPropertyChanged(nameof(IsChecked));
+            //}
             get { return isChecked; }
             set
             {
                 isChecked = value;
-                OnPropertyChanged();
                 ObservableCollection<Model> lm = new ObservableCollection<Model>();
+                foreach (TrelloObjectLabels aLabel in Download.labelsList)
+                    lm.Add(new Model(aLabel.CardLabel, isChecked)
+                    {
+                        Label = aLabel.CardLabel,
+                        IsChecked = isChecked
+                    });
                 var checkedLabel = from aLabel in lm
-                                  where aLabel.IsChecked
-                                  select aLabel;
+                                   where aLabel.IsChecked
+                                   select aLabel;
+                OnPropertyChanged(nameof(IsChecked));
             }
         }
 
